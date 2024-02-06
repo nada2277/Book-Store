e8c75856e769b24aee60290435538051200a870f
Nada Youssef <nraslan101@gmail.com>
2024-02-07 01:33:27 +0200

    testing

--- VisualProject/Program.cs
+++ VisualProject/Program.cs
@@ -1,20 +1,26 @@
 namespace VisualProject
 {
-  internal static class Program
-  {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    internal static class Program
     {
-      // To customize application configuration such as set high DPI settings or default font,
-      // see https://aka.ms/applicationconfiguration.
-      ApplicationConfiguration.Initialize();
-      Application.Run(new Form1());
-      #region test
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+            #region test
 
-      #endregion
+
+            Console.WriteLine("Hello From Nada ^^ ");
+
+
+            #endregion
+
+
+        }
     }
-  }
 }
