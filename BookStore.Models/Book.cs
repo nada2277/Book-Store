7bbece4459db74fdcf156e0e28716eed6ebc383b
Nada Youssef <nraslan101@gmail.com>
2024-02-13 16:29:24 +0200

    Adding bookimg for the path

--- BookStore.Models/Book.cs
+++ BookStore.Models/Book.cs
@@ -16,6 +16,7 @@ namespace BookStore.Models
         public int Stock { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+        public string BookImg { get; set; }
 
 
 
