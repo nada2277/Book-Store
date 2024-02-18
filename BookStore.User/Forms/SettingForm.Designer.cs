b8e25200bedf815e12c2b8b6a9a5d0d122bcf148
Nada Youssef <nraslan101@gmail.com>
2024-02-18 15:50:32 +0200

    labels of setting

--- BookStore.User/Forms/SettingForm.Designer.cs
+++ BookStore.User/Forms/SettingForm.Designer.cs
@@ -58,53 +58,48 @@
             label1.Location = new Point(0, 0);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(0, 20);
+            label1.Size = new Size(0, 15);
             label1.TabIndex = 0;
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(383, 156);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(335, 117);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(285, 52);
+            textBox1.Size = new Size(250, 40);
             textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(811, 156);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(710, 117);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(285, 52);
+            textBox2.Size = new Size(250, 40);
             textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(383, 305);
-            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Location = new Point(335, 229);
             textBox3.Multiline = true;
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(285, 52);
+            textBox3.Size = new Size(250, 40);
             textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(811, 305);
-            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Location = new Point(710, 229);
             textBox4.Multiline = true;
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(285, 52);
+            textBox4.Size = new Size(250, 40);
             textBox4.TabIndex = 4;
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(80, 191);
-            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Location = new Point(70, 143);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(240, 261);
+            pictureBox1.Size = new Size(210, 196);
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
             // 
@@ -112,46 +107,42 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Emoji", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(56, 35);
+            label2.Location = new Point(49, 26);
             label2.Name = "label2";
-            label2.Size = new Size(207, 80);
+            label2.Size = new Size(170, 64);
             label2.TabIndex = 6;
             label2.Text = "Profile";
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(383, 447);
-            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Location = new Point(335, 335);
             textBox5.Multiline = true;
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(285, 52);
+            textBox5.Size = new Size(250, 40);
             textBox5.TabIndex = 7;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(811, 447);
-            textBox6.Margin = new Padding(3, 4, 3, 4);
+            textBox6.Location = new Point(710, 335);
             textBox6.Multiline = true;
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(285, 52);
+            textBox6.Size = new Size(250, 40);
             textBox6.TabIndex = 8;
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(383, 596);
-            textBox7.Margin = new Padding(3, 4, 3, 4);
+            textBox7.Location = new Point(335, 447);
             textBox7.Multiline = true;
             textBox7.Name = "textBox7";
-            textBox7.Size = new Size(285, 52);
+            textBox7.Size = new Size(250, 40);
             textBox7.TabIndex = 9;
             // 
             // textBox8
             // 
-            textBox8.Location = new Point(811, 596);
-            textBox8.Margin = new Padding(3, 4, 3, 4);
+            textBox8.Location = new Point(710, 447);
             textBox8.Multiline = true;
             textBox8.Name = "textBox8";
-            textBox8.Size = new Size(285, 52);
+            textBox8.Size = new Size(250, 40);
             textBox8.TabIndex = 10;
             // 
             // button1
@@ -162,10 +153,9 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(611, 732);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(535, 549);
             button1.Name = "button1";
-            button1.Size = new Size(255, 75);
+            button1.Size = new Size(223, 56);
             button1.TabIndex = 11;
             button1.Text = "Save Changes";
             button1.UseVisualStyleBackColor = false;
@@ -174,57 +164,57 @@
             // lblFirstNameError
             // 
             lblFirstNameError.AutoSize = true;
-            lblFirstNameError.Location = new Point(383, 213);
+            lblFirstNameError.Location = new Point(335, 160);
             lblFirstNameError.Name = "lblFirstNameError";
-            lblFirstNameError.Size = new Size(0, 20);
+            lblFirstNameError.Size = new Size(0, 15);
             lblFirstNameError.TabIndex = 12;
             // 
             // lblLastNameError
             // 
             lblLastNameError.AutoSize = true;
-            lblLastNameError.Location = new Point(782, 213);
+            lblLastNameError.Location = new Point(710, 160);
             lblLastNameError.Name = "lblLastNameError";
-            lblLastNameError.Size = new Size(0, 20);
+            lblLastNameError.Size = new Size(0, 15);
             lblLastNameError.TabIndex = 13;
             // 
             // lblPasswordError
             // 
             lblPasswordError.AutoSize = true;
-            lblPasswordError.Location = new Point(383, 653);
+            lblPasswordError.Location = new Point(335, 490);
             lblPasswordError.Name = "lblPasswordError";
-            lblPasswordError.Size = new Size(0, 20);
+            lblPasswordError.Size = new Size(0, 15);
             lblPasswordError.TabIndex = 14;
             // 
             // lblEmailError
             // 
             lblEmailError.AutoSize = true;
-            lblEmailError.Location = new Point(782, 504);
+            lblEmailError.Location = new Point(710, 378);
             lblEmailError.Name = "lblEmailError";
-            lblEmailError.Size = new Size(0, 20);
+            lblEmailError.Size = new Size(0, 15);
             lblEmailError.TabIndex = 15;
             // 
             // lblUsernameError
             // 
             lblUsernameError.AutoSize = true;
-            lblUsernameError.Location = new Point(383, 504);
+            lblUsernameError.Location = new Point(335, 378);
             lblUsernameError.Name = "lblUsernameError";
-            lblUsernameError.Size = new Size(0, 20);
+            lblUsernameError.Size = new Size(0, 15);
             lblUsernameError.TabIndex = 16;
             // 
             // lblAddressError
             // 
             lblAddressError.AutoSize = true;
-            lblAddressError.Location = new Point(782, 367);
+            lblAddressError.Location = new Point(710, 275);
             lblAddressError.Name = "lblAddressError";
-            lblAddressError.Size = new Size(0, 20);
+            lblAddressError.Size = new Size(0, 15);
             lblAddressError.TabIndex = 17;
             // 
             // lblPhoneError
             // 
             lblPhoneError.AutoSize = true;
-            lblPhoneError.Location = new Point(383, 367);
+            lblPhoneError.Location = new Point(335, 275);
             lblPhoneError.Name = "lblPhoneError";
-            lblPhoneError.Size = new Size(0, 20);
+            lblPhoneError.Size = new Size(0, 15);
             lblPhoneError.TabIndex = 18;
             // 
             // button2
@@ -236,10 +226,9 @@
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Segoe UI", 22F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button2.ForeColor = SystemColors.ControlLightLight;
-            button2.Location = new Point(169, 405);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(148, 304);
             button2.Name = "button2";
-            button2.Size = new Size(69, 67);
+            button2.Size = new Size(60, 50);
             button2.TabIndex = 20;
             button2.Text = "+";
             button2.UseVisualStyleBackColor = false;
@@ -248,18 +237,18 @@
             // lblNewPasswordError
             // 
             lblNewPasswordError.AutoSize = true;
-            lblNewPasswordError.Location = new Point(782, 653);
+            lblNewPasswordError.Location = new Point(710, 490);
             lblNewPasswordError.Name = "lblNewPasswordError";
-            lblNewPasswordError.Size = new Size(0, 20);
+            lblNewPasswordError.Size = new Size(0, 15);
             lblNewPasswordError.TabIndex = 21;
             // 
             // SettingForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoScroll = true;
             BackColor = SystemColors.ControlLightLight;
-            ClientSize = new Size(1128, 841);
+            ClientSize = new Size(987, 631);
             Controls.Add(lblNewPasswordError);
             Controls.Add(button2);
             Controls.Add(lblPhoneError);
@@ -281,7 +270,7 @@
             Controls.Add(textBox6);
             Controls.Add(textBox8);
             Controls.Add(label1);
-            Margin = new Padding(2, 3, 2, 3);
+            Margin = new Padding(2);
             Name = "SettingForm";
             Text = "SettingForm";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
